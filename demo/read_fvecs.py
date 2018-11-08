bfae36404419e07b8dcc53dc15309406ce84ffbc
Dong <dwoanngg@gmail.com>
2018-11-08 02:35:14 +0000

    two more file

---
+++ demo/read_fvecs.py
@@ -0,0 +1,14 @@
+import os, sys
+import numpy as np
+def ivecs_read(fname):
+    a = np.fromfile(fname, dtype='int32')
+    d = a[0]
+    return a.reshape(-1, d + 1)[:, 1:].copy()
+
+
+def fvecs_read(fname):
+    return ivecs_read(fname).view('float32')
+
+data = fvecs_read(sys.argv[1])
+print(data[0])
+
